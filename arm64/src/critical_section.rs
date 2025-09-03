366524cdb7019919bf7bb91a03f65dd621e7d233
Moritz Meier <moritz.meier@dlr.de>
2025-09-03 12:51:06 +0200

    wip

--- arm64/src/critical_section.rs
+++ arm64/src/critical_section.rs
@@ -15,9 +15,15 @@ unsafe impl critical_section::Impl for CriticalSectionImpl {
                 "2:",
                 "wfe",
                 "3:",
+                "dsb sy",
+                "dc civac, {lock_addr}",
+                "isb sy",
                 "ldaxr {reg0}, [{lock_addr}]",
                 "cbnz {reg0}, 2b",
                 "stxr {reg1:w}, {reg2:x}, [{lock_addr}]",
+                "dsb sy",
+                "dc civac, {lock_addr}",
+                "isb sy",
                 "cbnz {reg1}, 3b",
                 lock_addr = in(reg) addr_of_mut!(GLOBAL_LOCK),
                 reg0 = out(reg) _,
@@ -31,6 +37,9 @@ unsafe impl critical_section::Impl for CriticalSectionImpl {
         unsafe {
             asm!(
                 "stlr xzr, [{lock_addr}]", // Store operation generates an event to all cores waiting in WFE
+                "dsb sy",
+                "dc civac, {lock_addr}",
+                "isb sy",
                 lock_addr = in(reg) addr_of_mut!(GLOBAL_LOCK),
             );
         }
